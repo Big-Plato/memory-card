d0c1b35005b132d96e06f4cbbea81680ebf9731d
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-28 15:41:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+import { useState } from 'react'
+
+export default function Header() {
+    const [count, setCount] = useState(0);
+    const [maxScore, setMaxScore] = useState(0);
+
+    return (
+        <div className="header">
+            <h1>Memory Game</h1>
+            <div className="score">
+                <p>Score: {count}</p>
+                <p>Max Score: {maxScore}</p>
+            </div>
+        </div>
+    )
+}
