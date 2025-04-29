ccaec762bd735abd1a25238dd1b6e1f28a4fd100
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-28 22:10:53 -0300

    feat: header module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
-import './Header.css'
-import { useState } from 'react'
+import "./Header.css";
+import { useState } from "react";
 
 export default function Header() {
-    const [count, setCount] = useState(0);
-    const [maxScore, setMaxScore] = useState(0);
+  const [count, setCount] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
 
-    return (
-        <div className="header">
-            <h1>Memory Game</h1>
-            <div className="score">
-                <p>Score: {count}</p>
-                <p>Max Score: {maxScore}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <h1>Memory Game</h1>
+      <div className="score">
+        <p>Score: {count}</p>
+        <p>Max Score: {maxScore}</p>
+      </div>
+    </div>
+  );
 }
