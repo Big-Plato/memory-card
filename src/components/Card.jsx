b66736dc389fb690e3fd2bf712e976c58919495b
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-30 14:22:43 -0300

    fix: id attribute for card to handleCardClick

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import "./Card.css";
 
-export default function Card({ onClick, name, img }) {
+export default function Card({ onClick, name, img, id }) {
   return (
-    <div className="card" onClick={onClick} >
+    <div className="card" onClick={onClick} id={id}>
       <div className="img-div">
-        <img src={img} />
+        <img id={id} src={img} />
       </div>
       <h2>{name}</h2>
     </div>
